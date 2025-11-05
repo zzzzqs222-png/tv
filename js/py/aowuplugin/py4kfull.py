6879bf506bfa15e880044baadcfb4dbc02bfcbc1
zzzzqs222-png <zzzzqs222@gmail.com>
2025-11-05 15:08:39 +0800

    Update py4kfull.py

--- js/py/aowuplugin/py4kfull.py
+++ js/py/aowuplugin/py4kfull.py
@@ -1,25 +1,40 @@
-# coding=utf-8
-# !/usr/bin/python
-# by嗷呜
+# -*- coding: utf-8 -*-
+# by @嗷呜
 import json
+import re
 import sys
 from base64 import b64decode, b64encode
-from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+import requests
 from pyquery import PyQuery as pq
 from requests import Session
 sys.path.append('..')
 from base.spider import Spider
 
-
 class Spider(Spider):
 
     def init(self, extend=""):
-        self.proxy = ''
-        if extend and json.loads(extend).get('proxy'):
-            self.proxy = json.loads(extend).get('proxy')
+        try:self.proxies = json.loads(extend)
+        except:self.proxies = {}
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5410.0 Safari/537.36',
+            'pragma': 'no-cache',
+            'cache-control': 'no-cache',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-ch-ua': '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
+            'dnt': '1',
+            'sec-ch-ua-mobile': '?0',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            'priority': 'u=1, i',
+        }
         self.host = self.gethost()
-        self.headers['referer'] = f'{self.host}/'
+        self.headers.update({'referer': f'{self.host}/', 'origin': self.host})
         self.session = Session()
+        self.session.proxies.update(self.proxies)
         self.session.headers.update(self.headers)
         pass
 
@@ -34,27 +49,6 @@ class Spider(Spider):
 
     def destroy(self):
         pass
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'sec-ch-ua': '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-full-version': '"133.0.6943.98"',
-        'sec-ch-ua-arch': '"x86"',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-ch-ua-platform-version': '"19.0.0"',
-        'sec-ch-ua-model': '""',
-        'sec-ch-ua-full-version-list': '"Not(A:Brand";v="99.0.0.0", "Google Chrome";v="133.0.6943.98", "Chromium";v="133.0.6943.98"',
-        'dnt': '1',
-        'upgrade-insecure-requests': '1',
-        'sec-fetch-site': 'none',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
-        'priority': 'u=0, i'
-    }
 
     def homeContent(self, filter):
         result = {}
@@ -76,7 +70,9 @@ class Spider(Spider):
         return result
 
     def homeVideoContent(self):
-        pass
+        data = self.getpq()
+        vhtml = data("#list_videos_videos_watched_right_now_items .item .lazyloaded")
+        return {'list': self.getlist(vhtml)}
 
     def categoryContent(self, tid, pg, filter, extend):
         vdata = []
@@ -85,163 +81,89 @@ class Spider(Spider):
         result['pagecount'] = 9999
         result['limit'] = 90
         result['total'] = 999999
-        if tid == '/latest-updates':
-            data = self.getpq(f'{tid}/{pg}')
-            jsdata = self.getjsdata(data)
-            jsdata.encoding = "utf-8"
-            res = jsdata.text
-            doc = BeautifulSoup(res, "lxml")
+        if tid in ['/latest-updates', '/top-rated', '/most-popular'] in tid:
+            data = self.getpq(f'{tid}{extend.get("type", "")}/{pg}')
+            vdata = self.getlist(data("#list_videos_videos_watched_right_now_items .item"))
 
-            soups = doc.find_all('div', class_="margin-fix")
-
-            for soup in soups:
-                vods = soup.find_all('div', class_="item")
-
-                for vod in vods:
-
-                    names = vod.find_all('a')
-                    name = names[0]['title']
-
-                    ids = vod.find_all('a')
-                    id = ids[0]['href']
-
-                    pics = vod.find('img', class_="lazyload")
-                    pic = pics['data-src']
-
-                    remarks = vod.find('div', class_="img thumb__img")
-                    remark = remarks.text.strip()
-
-                    vdata.append({
-                        "vod_id": id,
-                        "vod_name": name,
-                        "vod_pic": pic,
-                        "vod_remarks": remark
-                    })
-
-        elif tid == '/top-rated':
-            data = self.getpq(f'{tid}/{pg}')
-            jsdata = self.getjsdata(data)
-            jsdata.encoding = "utf-8"
-            res = jsdata.text
-            doc = BeautifulSoup(res, "lxml")
-
-            soups = doc.find_all('div', class_="margin-fix")
-
-            for soup in soups:
-                vods = soup.find_all('div', class_="item")
-
-                for vod in vods:
-
-                    names = vod.find_all('a')
-                    name = names[0]['title']
-
-                    ids = vod.find_all('a')
-                    id = ids[0]['href']
-
-                    pics = vod.find('img', class_="lazyload")
-                    pic = pics['data-src']
-
-                    remarks = vod.find('div', class_="img thumb__img")
-                    remark = remarks.text.strip()
-
-                    vdata.append({
-                        "vod_id": id,
-                        "vod_name": name,
-                        "vod_pic": pic,
-                        "vod_remarks": remark
-                    })
-
-        elif tid == '/most-popular':
-            data = self.getpq(f'{tid}/{pg}')
-            jsdata = self.getjsdata(data)
-            jsdata.encoding = "utf-8"
-            res = jsdata.text
-            doc = BeautifulSoup(res, "lxml")
-
-            soups = doc.find_all('div', class_="margin-fix")
-
-            for soup in soups:
-                vods = soup.find_all('div', class_="item")
-
-                for vod in vods:
-
-                    names = vod.find_all('a')
-                    name = names[0]['title']
-
-                    ids = vod.find_all('a')
-                    id = ids[0]['href']
-
-                    pics = vod.find('img', class_="lazyload")
-                    pic = pics['data-src']
-
-                    remarks = vod.find('div', class_="img thumb__img")
-                    remark = remarks.text.strip()
-
-                    vdata.append({
-                        "vod_id": id,
-                        "vod_name": name,
-                        "vod_pic": pic,
-                        "vod_remarks": remark
-                    })
         result['list'] = vdata
         return result
 
     def detailContent(self, ids):
+        url = f"{self.host}{ids[0]}"
         data = self.getpq(ids[0])
         vn = data('meta[property="og:title"]').attr('content')
-
         vod = {
             'vod_name': vn,
-            'vod_director': '',
-            'vod_remarks': '',
-            'vod_play_from': 'FullHD',
+            'vod_director': vn,
+            'vod_remarks': vn,
+            'vod_play_from': 'Pornhub',
             'vod_play_url': ''
         }
-
+        js_content = data("#player script").eq(0).text()
+        plist = [f"{vn}${self.e64(f'{1}@@@@{url}')}"]
         try:
-            # 获取视频播放列表
-            plist = [f"{vn}${self.e64(f'{1}@@@@{ids[0]}')}"]
-            vod['vod_play_url'] = '#'.join(plist)
+            pattern = r'"mediaDefinitions":\s*(\[.*?\]),\s*"isVertical"'
+            match = re.search(pattern, js_content, re.DOTALL)
+            if match:
+                json_str = match.group(1)
+                udata = json.loads(json_str)
+                plist = [
+                    f"{media['height']}${self.e64(f'{0}@@@@{url}')}"
+                    for media in udata[:-1]
+                    if (url := media.get('videoUrl'))
+                ]
         except Exception as e:
-            print(f"获取视频播放列表失败: {str(e)}")
-            vod['vod_play_url'] = f"{vn}${self.e64(f'{1}@@@@{ids[0]}')}"
-
+            print(f"提取mediaDefinitions失败: {str(e)}")
+        vod['vod_play_url'] = '#'.join(plist)
         return {'list': [vod]}
 
     def searchContent(self, key, quick, pg="1"):
-        data = self.getpq(f'/search/{key}?page={pg}')
-        return {'list': self.getlist(data(".thumb-list--sidebar .thumb-list__item")), 'page': pg}
+        data = self.getpq(f'/video/search?search={key}&page={pg}')
+        return {'list': self.getlist(data('#videoSearchResult .pcVideoListItem .phimage'))}
 
     def playerContent(self, flag, id, vipFlags):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5410.0 Safari/537.36',
-            'pragma': 'no-cache',
-            'cache-control': 'no-cache',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-ch-ua': '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
-            'dnt': '1',
-            'sec-ch-ua-mobile': '?0',
-            'origin': self.host,
-            'sec-fetch-site': 'cross-site',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-dest': 'empty',
-            'referer': f'{self.host}/',
-            'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
-            'priority': 'u=1, i',
-        }
         ids = self.d64(id).split('@@@@')
-        return {'parse': int(ids[0]), 'url': f'{self.proxy}{ids[1]}', 'header': headers}
+        if '.m3u8' in ids[1]: ids[1] = self.proxy(ids[1], 'm3u8')
+        return {'parse': int(ids[0]), 'url': ids[1], 'header': self.headers}
 
     def localProxy(self, param):
-        pass
+        url = self.d64(param.get('url'))
+        if param.get('type') == 'm3u8':
+            return self.m3Proxy(url)
+        else:
+            return self.tsProxy(url)
+
+    def m3Proxy(self, url):
+        ydata = requests.get(url, headers=self.headers, proxies=self.proxies, allow_redirects=False)
+        data = ydata.content.decode('utf-8')
+        if ydata.headers.get('Location'):
+            url = ydata.headers['Location']
+            data = requests.get(url, headers=self.headers, proxies=self.proxies).content.decode('utf-8')
+        lines = data.strip().split('\n')
+        last_r = url[:url.rfind('/')]
+        parsed_url = urlparse(url)
+        durl = parsed_url.scheme + "://" + parsed_url.netloc
+        for index, string in enumerate(lines):
+            if '#EXT' not in string:
+                if 'http' not in string:
+                    domain = last_r if string.count('/') < 2 else durl
+                    string = domain + ('' if string.startswith('/') else '/') + string
+                lines[index] = self.proxy(string, string.split('.')[-1].split('?')[0])
+        data = '\n'.join(lines)
+        return [200, "application/vnd.apple.mpegur", data]
+
+    def tsProxy(self, url):
+        data = requests.get(url, headers=self.headers, proxies=self.proxies, stream=True)
+        return [200, data.headers['Content-Type'], data.content]
 
     def gethost(self):
         try:
-            response = self.fetch(f'{self.proxy}https://www.fullhd.xxx', headers=self.headers, allow_redirects=False)
-            return response.headers['Location']
+            response = requests.get('https://www.fullhd.xxx/zh', headers=self.headers, proxies=self.proxies,
+                                    allow_redirects=False)
+            return response.headers['Location'][:-1]
         except Exception as e:
             print(f"获取主页失败: {str(e)}")
-            return "https://www.fullhd.xxx/zh"
+            return "https://www.fullhd.xxx"
 
     def e64(self, text):
         try:
@@ -265,22 +187,22 @@ class Spider(Spider):
         vlist = []
         for i in data.items():
             vlist.append({
-                'vod_id': i('.thumb').attr('href'),
-                'vod_name': i('.thumb-title').text(),
-                'vod_pic': i('img').attr('src'),
+                'vod_id': i('a').attr('href'),
+                'vod_name': i('a').attr('title'),
+                'vod_pic': i('lazyloaded').attr('src'),
                 'vod_remarks': i('.duration').text(),
                 'style': {'ratio': 1.33, 'type': 'rect'}
             })
         return vlist
 
-    def getpq(self, path=''):
-        h = '' if path.startswith('http') else self.host
-        response = self.session.get(f'{self.proxy}{h}{path}').text
+    def getpq(self, path):
         try:
-            return pq(response)
-        except Exception as e:
-            print(f"{str(e)}")
+            response = self.session.get(f'{self.host}{path}').text
             return pq(response.encode('utf-8'))
+        except Exception as e:
+            print(f"请求失败: , {str(e)}")
+            return None
 
-    def getjsdata(self, data):
-        return {}
+    def proxy(self, data, type='img'):
+        if data and len(self.proxies):return f"{self.getProxyUrl()}&url={self.e64(data)}&type={type}"
+        else:return data
